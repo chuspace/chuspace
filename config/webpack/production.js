c93e8a632c92fa2e86b577e5b698d71c91d86993
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2022-03-27 12:28:15 +0100

    Replace terser

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,5 +1,39 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const webpackConfig = require('./base')
+const TerserPlugin = require('terser-webpack-plugin')
+const { merge } = require('@rails/webpacker')
 
-module.exports = webpackConfig
+webpackConfig.optimization.minimizer = webpackConfig.optimization.minimizer.filter(
+  (plugin) =>
+    plugin.constructor && plugin.constructor.name === 'CssMinimizerPlugin'
+)
+
+const productionConfig = {
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        parallel: Number.parseInt(process.env.WEBPACKER_PARALLEL, 10) || true,
+        terserOptions: {
+          keep_classnames: /Element$/,
+          parse: {
+            ecma: 8
+          },
+          compress: {
+            ecma: 5,
+            warnings: false,
+            comparisons: false
+          },
+          mangle: { safari10: true },
+          output: {
+            ecma: 5,
+            comments: false,
+            ascii_only: true
+          }
+        }
+      })
+    ]
+  }
+}
+
+module.exports = merge(webpackConfig, productionConfig)
