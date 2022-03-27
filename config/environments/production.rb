28669af262bd837bcc7067de80a0b352df0aad74
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2022-03-27 12:36:33 +0100

    Use custom host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,6 +34,9 @@ Rails.application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
+  config.action_mailer.default_url_options = { host: 'chuspace.com', protocol: 'https' }
+  config.default_url_options = { host: 'chuspace.com' }
+  Rails.application.routes.default_url_options[:host] = 'chuspace.com'
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for Apache
