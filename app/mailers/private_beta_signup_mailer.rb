91bde6293adf8f49cf1b1dc300b920cf26005228
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2022-03-27 11:54:11 +0100

    Send it to chuspace email

--- app/mailers/private_beta_signup_mailer.rb
+++ app/mailers/private_beta_signup_mailer.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
 class PrivateBetaSignupMailer < ApplicationMailer
+  RECIPIENT = 'signups@chuspace.com'
+
   def notify
     @identity = params[:identity]
     @user = @identity.user
 
     @subject = t('.subject', name: @user.name)
-    mail(to: @user.email, subject: @subject)
+    mail(to: RECIPIENT, subject: @subject)
   end
 end
