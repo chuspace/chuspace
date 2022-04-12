28b6322701047c6ffbfe2729a64cd56dbc015ad5
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2022-04-12 09:55:48 +0100

    Add replica support

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,6 +2,7 @@
 
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+  connects_to database: { writing: :primary, reading: :primary_replica }
 
   def valid_attributes?(*attributes)
     errors.clear
