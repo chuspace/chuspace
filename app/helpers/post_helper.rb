46d12d3d061901ea6bd0f2e63fb4ff7e96bebc14
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2022-05-18 17:04:37 +0100

    Add some basic sharing options

--- app/helpers/post_helper.rb
+++ app/helpers/post_helper.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
 module PostHelper
-  def edit_or_show_post_path(post:)
-    if post.published?
-      user_publication_post_path(post.publication.owner, post.publication, post.editions.current)
-    else
-      edit_user_publication_post_path(post.publication.owner, post.publication, post.revisions.current)
+  def social_share_url(post:, provider:)
+    case provider.to_sym
+    when :facebook then "https://www.facebook.com/sharer.php?u=#{publication_post_url(post.publication, post)}"
+    when :twitter then "https://twitter.com/intent/tweet?url=#{publication_post_url(post.publication, post)}"
+    when :linkedin then "https://www.linkedin.com/shareArticle?url=#{publication_post_url(post.publication, post)}"
+    when :email then "mailto:?subject=#{post.title}&body=#{publication_post_url(post.publication, post)}"
     end
   end
 end
