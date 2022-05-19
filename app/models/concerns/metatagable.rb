92636c1cccfa7bf14176e84f2a30e1bcc6c82745
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2022-05-19 18:15:15 +0100

    Rubocop

--- app/models/concerns/metatagable.rb
+++ app/models/concerns/metatagable.rb
@@ -62,8 +62,8 @@ module Metatagable
 
   def og_image_url
     @og_image_url ||= case self
-    when Publication then icon.variant(:profile)&.processed&.url
-    when Post then featured_image&.image&.variant(:social)&.processed&.url
+                      when Publication then icon.variant(:profile)&.processed&.url
+                      when Post then featured_image&.image&.variant(:social)&.processed&.url
     end
   end
 
