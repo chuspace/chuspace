aa4214b0a167806dc131bc320318d69ad206416e
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2022-05-26 10:05:39 +0100

    Remove logging

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,11 +55,6 @@ Rails.application.configure do
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
 
-  puts 'env'
-  puts ENV['RAILS_MASTER_KEY']
-  puts Rails.application.credentials.inspect
-  puts Rails.env
-
   config.action_mailer.smtp_settings = {
     address: 'smtp.eu.sparkpostmail.com',
     port: 587,
