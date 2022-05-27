36c8cedaec1bf7f88345863142f7c8bff799e8f7
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2022-05-27 17:23:44 +0100

    Disable cookies for ahoy

--- config/initializers/ahoy.rb
+++ config/initializers/ahoy.rb
@@ -6,11 +6,10 @@ end
 # set to true for JavaScript tracking
 Ahoy.api = false
 
-# set to true for geocoding (and add the geocoder gem to your Gemfile)
-# we recommend configuring local geocoding as well
-# see https://github.com/ankane/ahoy#geocoding
 Ahoy.geocode = false
-Ahoy.job_queue = :low
+Ahoy.job_queue = :default
 Ahoy.visit_duration = 24.hours
 Safely.report_exception_method = ->(exception) { Raven::Rack.capture_exception(exception) }
 Ahoy.server_side_visits = false
+Ahoy.cookies = false
+Ahoy.mask_ips = true
