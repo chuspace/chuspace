e7e6cb4050906dc719302b7834eec6e64a618088
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2022-05-30 15:48:24 +0100

    Upgrade gems

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Sentry.init do |config|
+  config.breadcrumbs_logger = %i[active_support_logger http_logger]
   config.dsn = Rails.application.credentials.sentry[:dsn]
   config.traces_sample_rate = 0.5
 end
