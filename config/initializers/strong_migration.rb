48307ad8493396d15d4cad7c81f57f63bb6a78ec
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2022-05-30 22:07:39 +0100

    Add strong migration options

---
+++ config/initializers/strong_migration.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+StrongMigrations.lock_timeout             = 10.seconds
+StrongMigrations.statement_timeout        = 1.hour
+StrongMigrations.lock_timeout_retries     = 3
+StrongMigrations.lock_timeout_retry_delay = 10.seconds
