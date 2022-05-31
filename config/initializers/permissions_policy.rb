8a4e67908bf658a2a52a8c33869614225d68d723
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2022-05-31 17:48:51 +0100

    Set permission policy

--- config/initializers/permissions_policy.rb
+++ config/initializers/permissions_policy.rb
@@ -1,12 +1,10 @@
 # frozen_string_literal: true
-# Define an application-wide HTTP permissions policy. For further
-# information see https://developers.google.com/web/updates/2018/06/feature-policy
-#
-# Rails.application.config.permissions_policy do |f|
-#   f.camera      :none
-#   f.gyroscope   :none
-#   f.microphone  :none
-#   f.usb         :none
-#   f.fullscreen  :self
-#   f.payment     :self, "https://secure.example.com"
-# end
+
+Rails.application.config.permissions_policy do |permission|
+  permission.camera      :none
+  permission.gyroscope   :none
+  permission.microphone  :none
+  permission.usb         :none
+  permission.fullscreen  :self
+  permission.payment     :none
+end
