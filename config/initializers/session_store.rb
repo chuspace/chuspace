fce851650a999d471921dfc444d3b4fa2a2a9844
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2022-06-01 16:34:36 +0100

    Change key just in case

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -4,7 +4,10 @@ require_relative '../../app/models/server_side_session.rb'
 
 Rails.application.config.session_store(
   :active_record_store,
-  key: '_chuspace_global_session'
+  domain: :all,
+  secure: Rails.env.produdction?,
+  serializer: :json,
+  key: '_chuspace_app_global_session'
 )
 
 ActionDispatch::Session::ActiveRecordStore.session_class = ServerSideSession
