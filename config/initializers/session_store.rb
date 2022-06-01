04b424c84ef4dc4cff470b243f3b094e1b57947e
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2022-06-01 16:26:54 +0100

    Remove domain from cookie

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -4,8 +4,6 @@ require_relative '../../app/models/server_side_session.rb'
 
 Rails.application.config.session_store(
   :active_record_store,
-  domain: :all,
-  serializer: :json,
   key: '_chuspace_global_session'
 )
 
