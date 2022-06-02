3dd6087403b4b76f30615b0f29f00e675900043b
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2022-06-02 18:57:50 +0100

    Swap to redis session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,13 @@
 # frozen_string_literal: true
 
-require_relative '../../app/models/server_side_session.rb'
-
-Rails.application.config.session_store :active_record_store, key: '_chuspace_session'
-ActionDispatch::Session::ActiveRecordStore.session_class = ServerSideSession
+Rails.application.config.session_store :redis_session_store,
+  key: '_chuspace_session',
+  serializer: :hybrid,
+  on_redis_down: ->(exception, env, sid) { Sentry.capture_exception(exception, env) },
+  on_session_load_error: ->(e, sid) { Sentry.capture_exception(exception, env) },
+  redis: {
+    expire_after: 1.week,
+    ttl: 1.week,
+    key_prefix: "chuspace_#{Rails.env}_session:",
+    url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/1'),
+  }
