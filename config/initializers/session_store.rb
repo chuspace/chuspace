f91179cd4c00707c1c0a58a62c03114ef46f2eef
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2022-06-02 17:23:09 +0100

    Disable server side cookies

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
-require_relative '../../app/models/server_side_session.rb'
+# require_relative '../../app/models/server_side_session.rb'
 
-Rails.application.config.session_store(
-  :active_record_store,
-  domain: :all,
-  secure: Rails.env.production?,
-  serializer: :json,
-  key: '_chuspace_app_global_session'
-)
+# Rails.application.config.session_store(
+#   :cookie_store,
+#   domain: :all,
+#   secure: Rails.env.production?,
+#   serializer: :json,
+#   key: '_chuspace_app_global_session'
+# )
 
-ActionDispatch::Session::ActiveRecordStore.session_class = ServerSideSession
+# ActionDispatch::Session::ActiveRecordStore.session_class = ServerSideSession
