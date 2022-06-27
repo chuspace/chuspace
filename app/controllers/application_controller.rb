f93d41c290fe3a4e7551e2a3873088c89d7b59af
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2022-06-27 20:24:40 +0530

    Fix typo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,7 @@ class ApplicationController < ActionController::Base
     super
 
     payload[:host] = request.host
-    paylod[:x_forwarded_for] = request.env['HTTP_X_FORWARDED_FOR']
+    payload[:x_forwarded_for] = request.env['HTTP_X_FORWARDED_FOR']
   end
 
   private
