0bdbf95f811e38044f835ff89735107d85011149
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2022-06-30 20:19:16 +0530

    Enable default invoke

--- config/application.rb
+++ config/application.rb
@@ -45,9 +45,6 @@ module Chuspace
     # Custom error pages
     config.exceptions_app = self.routes
 
-    # Configure maybe_later
-    config.invoke_even_if_server_is_unsupported = true
-
     # Autoload paths
     config.anyway_config.autoload_static_config_path = 'app/configs'
     overrides = "#{root}/app/overrides"
