20b07090fe7ce57e8534376e5d99f4da690b05af
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2022-06-30 20:26:53 +0530

    Add production creds

---
+++ config/initializers/maybe_later.rb
@@ -0,0 +1,18 @@
+MaybeLater.config do |config|
+    # Will be called if a block passed to MaybeLater.run raises an error
+    config.on_error = ->(exception) { Rollbar.error(exception) }
+
+    # Will run after each `MaybeLater.run {}` block, even if it errors
+    config.after_each = -> {}
+
+    # By default, tasks will run in a fixed thread pool. To run them in the
+    # thread dispatching the HTTP response, set this to true
+    config.inline_by_default = false
+
+    # How many threads to allocate to the fixed thread pool (default: 5)
+    config.max_threads = 5
+
+    # If set to true, will invoke the after_reply tasks even if the server doesn't
+    # provide an array of rack.after_reply array
+    config.invoke_even_if_server_is_unsupported = true
+  end
