cfea7e91ee648ebf2baa73624d48b7e5142cbf4d
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2022-09-14 15:57:13 +0530

    Make email message ambigous

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,23 +13,8 @@ class SessionsController < ApplicationController
   def create
     @identity = Identity.find_by(provider: :email, uid: session_params[:email])
 
-    if @identity
-      @identity.send_magic_auth_email!
-      redirect_to redirect_location_for(:user) || root_path, notice: t('sessions.create.success')
-    else
-      @user = User.new(email: session_params[:email])
-
-      if user = User.find_by(email: session_params[:email])
-        @user.errors.add(:email, :identity_taken, provider: user.identities.pluck(:provider).to_sentence)
-      else
-        @user.errors.add(:email, :no_email_identity)
-      end
-
-      respond_to do |format|
-        format.html { redirect_to email_sessions_path, error: t('sessions.create.failure') }
-        format.turbo_stream { render turbo_stream: turbo_stream.replace(@user, partial: 'sessions/form', locals: { user: @user }) }
-      end
-    end
+    @identity.send_magic_auth_email! if @identity.present?
+    redirect_to redirect_location_for(:user) || root_path, notice: t('sessions.create.confirm')
   end
 
   def destroy
